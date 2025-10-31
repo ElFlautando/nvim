25a97d2fab83eb50fe33dede2461f5db92a3c69a
lfs <lfs>
2025-10-31 11:40:34 +0100

    lsp keymaps + oil

--- lua/config/keymapping.lua
+++ lua/config/keymapping.lua
@@ -3,7 +3,9 @@ local M = {}
 
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { noremap = true, silent = true, })
 
-vim.keymap.set("n", "wq" , ":wq")
+vim.keymap.set("n", "wq" , ":wq<CR>")
+
+vim.keymap.set("n", "<leader>o", ":Oil<CR>", {noremap = true, silent =true})
 
 
 -- Command keymap:
@@ -16,6 +18,13 @@ vim.keymap.set("c", "<C-e>", "<End>", { noremap = true, })
 
 -- Change keymap
 --
+
+function M.lsp()
+  vim.keymap.set("n", "<leader>fm", vim.lsp.buf.format())
+  vim.keymap.set("n", "<leader>ac" , vim.lsp.buf.code_action())
+
+  vim.keymap.set("n", "<leader>dg", vim.diagnostics.open_float())
+end
 
 function M.hover()
   vim.keymap.set('n', 'K', function()
