9e677e9782875cf40eec68974ea9d3a25f9f791c
lfs <lfs>
2025-11-11 22:07:24 +0100

    Copy paste

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,4 +1,3 @@
-
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.expandtab = true
@@ -9,13 +8,28 @@ vim.o.clipboard = "unnamedplus"
 
 vim.diagnostic.config {
   virtual_text = false,
-  float = { show_header = true, source = 'always', border = 'rounded', focusable = false,},
+  float = { show_header = true, source = 'always', border = 'rounded', focusable = false, },
 }
 
 local signs = { Error = " ", Warn = " ", Hint = " ", Info = " ", }
 for type, icon in pairs(signs) do
-    local hl = "DiagnosticSign" .. type
-    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "", })
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "", })
 end
 
 vim.cmd("colorscheme tokyonight")
+
+if os.getenv "TMUX" ~= nil then
+  vim.g.clipboard = {
+    name = "tmux clipboard",
+    copy = {
+      ["+"] = { "tmux", "load-buffer", "-" },
+      ["*"] = { "tmux", "load-buffer", "-" },
+    },
+    paste = {
+      ["+"] = { "tmux", "save-buffer", "-" },
+      ["*"] = { "tmux", "save-buffer", "-" },
+    },
+    cache_enabled = true,
+  }
+end
