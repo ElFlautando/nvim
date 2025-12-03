8a25d7c181e8b6e60d63ddcf92cb20e93448f4f0
Defaultuser <none>
2025-12-03 14:00:49 +0100

    Added cmp config

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,3 +1,115 @@
-return{
-  
+return {
+  "hrsh7th/nvim-cmp",
+  dependencies = {
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-cmdline",
+    "hrsh7th/cmp-path",
+    "saadparwaiz1/cmp_luasnip",
+    "L3MON4D3/LuaSnip",
+  },
+
+  opts = function(_, opts)
+    local cmp = require("cmp")
+
+    vim.opt.completeopt = { "menuone", "noinsert", "noselect" }
+    vim.g.cmp_disable_enable_toggle = false
+
+    opts.enabled = function()
+      return vim.g.cmp_disable_enable_toggle
+    end
+
+    opts.snippet = {
+      expand = function(args)
+        require("luasnip").lsp_expand(args.body)
+      end,
+    }
+
+    opts.sources = cmp.config.sources({
+      { name = "nvim_lsp" },
+      { name = "luasnip" },
+    }, {
+      { name = "buffer" },
+      { name = "treesitter" },
+    })
+
+    opts.sorting = {
+      comparators = {
+        cmp.config.compare.offset,
+        cmp.config.compare.exact,
+        cmp.config.compare.recently_used,
+        require("clangd_extensions.cmp_scores"),
+        cmp.config.compare.kind,
+        cmp.config.compare.sort_text,
+        cmp.config.compare.length,
+        cmp.config.compare.order,
+      },
+    }
+
+    local t = function(str)
+      return vim.api.nvim_replace_termcodes(str, true, true, true)
+    end
+
+    opts.mapping = vim.tbl_extend("force", opts.mapping or {}, {
+      ["<CR>"] = cmp.mapping.confirm({ select = true }),
+
+      ["<C-n>"] = cmp.mapping({
+        c = function()
+          if cmp.visible() then
+            cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
+          else
+            vim.api.nvim_feedkeys(t("<Down>"), "n", true)
+          end
+        end,
+        i = function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
+          else
+            fallback()
+          end
+        end,
+      }),
+
+      ["<C-p>"] = cmp.mapping({
+        c = function()
+          if cmp.visible() then
+            cmp.select_prev_item({ behavior = cmp.SelectBehavior.Insert })
+          else
+            vim.api.nvim_feedkeys(t("<Up>"), "n", true)
+          end
+        end,
+        i = function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item({ behavior = cmp.SelectBehavior.Insert })
+          else
+            fallback()
+          end
+        end,
+      }),
+
+      ["<C-e>"] = cmp.mapping({
+        i = cmp.mapping.close(),
+        c = cmp.mapping.close(),
+      }),
+    })
+    -- Cmdline completion
+    cmp.setup.cmdline({ "/", "?" }, {
+      enabled = false,
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = { { name = "buffer" } },
+    })
+
+    cmp.setup.cmdline(":", {
+      enabled = false,
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources(
+        { { name = "path" } },
+        { { name = "cmdline" } }
+      ),
+      matching = { disallow_symbol_nonprefix_matching = false },
+    })
+
+
+    return opts
+  end,
 }
