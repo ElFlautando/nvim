2e78751dea466068cbc97dbf442325e0a5904d12
lfs <lfs>
2025-12-19 12:29:16 +0100

    Added extra flags for clangd

--- lua/lsp/clangd.lua
+++ lua/lsp/clangd.lua
@@ -1,1 +1,27 @@
+vim.lsp.config.clangd = {
+  filetypes = { "c", "cpp", "objc", "objcpp", "cuda", },
+  cmd = {
+    -- see clangd --help-hidden
+    "clangd",
+    "--background-index",
+    "--clang-tidy",
+    "--all-scopes-completion",
+    "--header-insertion-decorators",
+    -- "--experimental-modules-support", -- this breaks the single file
+    "--pch-storage=memory",
+    "--completion-style=bundled",
+    "--cross-file-rename",
+    "--header-insertion=iwyu",
+  },
+  init_options = {
+    clangdFileStatus = true,     -- Provides information about activity on clangd’s per-file worker thread
+    usePlaceholders = true,
+    completeUnimported = true,
+    semanticHighlighting = true,
+    fallbackFlags = { "-std=c++23", },
+  },
+  -- on_attach = function(client, bufn) client.server_capabilities.semanticTokensProvider = nil end,
+  root_markers = { ".clangd", ".git", "compile_commands.json", },
+  single_file_support = true,
+}
 vim.lsp.enable "clangd"
