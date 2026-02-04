4ff66d294bbda44d69318a7ba715fe0bee3fe040
lfs <lfs>
2026-02-04 01:30:53 +0100

    Added spellchecks

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -1,7 +1,7 @@
 return {
   "mason-org/mason-lspconfig.nvim",
   opts = {
-    ensure_installed = { "lua_ls", "clangd",  "pyright", },
+    ensure_installed = { "lua_ls", "clangd",  "pyright", "cspell-lsp", "esbonio", "ltex_plus" },
     automatic_installation = true,
   },
   dependencies = {
